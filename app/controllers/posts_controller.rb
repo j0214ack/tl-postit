65b3977efa608a5caa330656b44ff5573cb9d31e
Jack Chen <b98207070@ntu.edu.tw>
2015-05-10 23:11:42 -0500

    Post workflow done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,53 @@
 class PostsController < ApplicationController
-  def index
 
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = User.first
+
+    if @post.save
+      # redirect_to post_path(@post)
+      flash[:notice] = "You've successfully created this post."
+      redirect_to posts_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @post.user = User.first
+    if @post.update(post_params)
+      # redirect_to post_path(@post)
+      flash[:notice] = "You've successfully edited this post."
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :url)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
